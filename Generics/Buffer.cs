f80b51d2147d17e7abb7691f74742f1b4146d80f
Gaind Laurenz <laurenz.gaind@sz-ybbs.ac.at>
2019-02-14 09:39:19 +0100

    Init

---
+++ Generics/Buffer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Buffer
+    {
+        private ArrayList data = new ArrayList();
+        public void Put(object x)
+        {
+            data.Add(x);
+        }
+        public object Get(int index)
+        {
+            return data[index];
+        }
+    }
+}
---
+++ Generics/TestBuffer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class TestBuffer
+    {
+        public static void BoxingUnboxing()
+        {
+            Console.WriteLine("\nBeispiel 1: Boxing/Unboxing.. ");
+            Buffer buf = new _0.Buffer();
+            buf.Put(3);
+            buf.Put(new Rectangle(10, 10, 20, 20));
+            int i1 = (int)buf.Get(0);
+            Console.WriteLine("Das 1. Element aus dem Buffer ist: " + i1);
+            try
+            {
+                int i2 = (int)buf.Get(1);
+            }
+            catch (TypeAccessException)
+            {
+                Console.WriteLine("Fehler bei Typzuweisung: int i2 = (int)buf.Get(1); ");
+            }
+            Object o1 = buf.Get(1);
+            if (o1.GetType() == (Rectangle))
+            {
+                Rectangle r1 = (Rectangle)o1;
+                Console.WriteLine("Der Objekt Rectangle wurde mit Typprüfung ausgelesen.");
+            }
+        }
+    }
+}
