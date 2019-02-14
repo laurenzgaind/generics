450549ea0d64ea47e11bcc274dd23e1fe69eeefe
Gaind Laurenz <laurenz.gaind@sz-ybbs.ac.at>
2019-02-14 10:16:46 +0100

    Buffer<Element>

--- Generics/Aufgabe3.cs
+++ Generics/Aufgabe3.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Generics
 {
-    class Aufgabe3
+    class Buffer<Element>
     {
+        private List<Element> data = new List<Element>();
+        public void Put(Element x)
+        {
+            data.Add(x);
+        }
+        public Element Get(int index)
+        {
+            return data[index];
+        }
     }
 }
